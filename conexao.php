17fbd8666a070caf79fc5132663cfb9f76673a97
hemkdev <lucas.hemkemeier007@gmail.com>
2025-09-19 08:35:02 -0300

    add  files via upload

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<?php
+ 
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "root";
+    $dbname = "projeto_curso";
+
+    $conexao = new mysqli($servidor, $usuario, $senha, $dbname);
+
+    // Verificar conexão
+    if ($conexao->connect_error) {
+        die("Falha na conexão: " . $conn->connect_error);    
+    }
+
+    $conexao->set_charset("utf8");
+?>
---
+++ navbar.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-dark bg-dark">
+	<div class="container-md">
+		<a class="navbar-brand" href="#"> Projeto - Fábrica de software </a>
+	</div>
+</nav>
