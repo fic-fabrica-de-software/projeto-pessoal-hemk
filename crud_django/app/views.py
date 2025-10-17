08882db88dd5835cac0d2944a5911806f1e5fb09
hemkdev <lucas_l_hemkemeier@estudante.sesisenai.org.br>
2025-10-17 16:02:46 -0300

    feat: login e registro funcionais!

--- crud_django/app/views.py
+++ crud_django/app/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Usuario
 
-def home(request):
+def login(request):
     return render(request, 'usuarios/login.html')
+
+def cadastro(request):
+    return render(request, 'usuarios/cadastro.html')
 
 def cadastrar_usuario(request):
     if request.method == 'POST':
@@ -11,7 +14,18 @@ def cadastrar_usuario(request):
         novo_usuario.senha = request.POST.get('senha')
         novo_usuario.save()
         return redirect('listagem_usuarios')
-    return redirect('home') # Redireciona para a página de login se for GET
+    return redirect('login') # Redireciona para a página de login se for GET
+
+def logar_usuario(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        senha = request.POST.get('senha')
+        try:
+            usuario = Usuario.objects.get(nome=nome, senha=senha)
+            return redirect('listagem_usuarios')
+        except Usuario.DoesNotExist:
+            return redirect('login') # Redireciona para a página de login se falhar
+    return redirect('login') # Redireciona para a página de login se for GET
 
 def listagem_usuarios(request):
     usuarios_cadastrados = Usuario.objects.all()
@@ -19,3 +33,8 @@ def listagem_usuarios(request):
         'usuarios': usuarios_cadastrados
     }
     return render(request, 'usuarios/usuarios.html', contexto)
+
+def deletar_usuario(request, usuario_id):
+    usuario = get_object_or_404(Usuario, id=usuario_id)
+    usuario.delete()
+    return redirect('listagem_usuarios')
